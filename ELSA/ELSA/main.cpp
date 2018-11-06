ce169d977af954e8d9900119ac1c7f17f3ce5d05
green-pirate <green-pirate@github.com>
2018-11-06 11:28:50 +0100

    merge main files

--- ELSA/ELSA/main.cpp
+++ ELSA/ELSA/main.cpp
@@ -1,4 +1,6 @@
 #include "./Atom/Atom.h"
+#include "material.h"
+#include "parameters.h"
 #include <iostream>
 
 using namespace std;
@@ -15,6 +17,34 @@ int main()
 
 	cout << "Fast nu har vi en atom med ID " << a.getID() << ", position (" << a.getPosX() << ", " << a.getPosY() << ", " << a.getPosZ() << ") och hastighet (" << a.getVelocityX() << ", " << a.getVelocityY() << ", " << a.getVelocityZ() << ")!" << endl;
 	
+
+
+	Material myMaterial;
+	Parameters myParameters;
+
+	myMaterial.setCrystalStructure("FCC");
+	myMaterial.setEpsilon(3.1415926536);
+
+	myParameters.setIsThermostatOn(true);
+	myParameters.setChosenMaterial(myMaterial);
+	myParameters.setCollisionFrequency(10.05);
+
+	cout << myParameters.getIsThermostatOn() << endl;
+	cout << myParameters.getCollisionFrequency() << endl;
+
+	cout << myParameters.getChosenMaterial().getCrystalStructure() << endl;
+	cout << myParameters.getChosenMaterial().getEpsilon() << endl;
+
+	Material anotherMaterial("BCC", 4.5, 1.2, 2.3, 4.5, 55);
+	Parameters anotherParameters(100, 10, 1000, 4.56, 1.00, true, true, anotherMaterial);
+
+	cout << anotherParameters.getIsThermostatOn() << endl;
+	cout << anotherParameters.getCollisionFrequency() << endl;
+
+	cout << anotherParameters.getChosenMaterial().getCrystalStructure() << endl;
+	cout << anotherParameters.getChosenMaterial().getEpsilon() << endl;
+
+
 
 	char exit;
 
