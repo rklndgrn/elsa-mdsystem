1aa2e8cf4c182b7aabc5223c1f3ff673a0319663
green-pirate <green-pirate@github.com>
2018-11-06 10:41:55 +0100

    add parameters class

--- ELSA/ELSA/main.cpp
+++ ELSA/ELSA/main.cpp
@@ -1,10 +1,29 @@
 #include "material.h"
+#include "parameters.h"
 #include <iostream>
+
+using namespace std;
 
 int main()
 { 
 	Material myMaterial;
-	while (1==1)
-		std::cout << myMaterial.getMass() << std::endl;
+	Parameters myParameters;
+
+	myMaterial.setCrystalStructure("FCC");
+	myMaterial.setEpsilon(3.1415926536);
+
+	myParameters.setIsThermostatOn(true);
+	myParameters.setChosenMaterial(myMaterial);
+	myParameters.setCollisionFrequency(10.05);
+
+	cout << myParameters.getIsThermostatOn() << endl;
+	cout << myParameters.getCollisionFrequency() << endl;
+
+	cout << myParameters.getChosenMaterial().getCrystalStructure() << endl;
+	cout << myParameters.getChosenMaterial().getEpsilon() << endl;
+
+	char exit;
+	cin >> exit;
+
 	return 0; 
 }
