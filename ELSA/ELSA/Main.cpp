fd1943418b4b4903e7f5d25a98ae1310ec667531
Erik Sandlund <erisa920@student.liu.se>
2018-11-06 11:21:01 +0100

    Lade till grannlistan.

--- ELSA/ELSA/Main.cpp
+++ ELSA/ELSA/Main.cpp
@@ -6,14 +6,21 @@ using namespace std;
 int main()
 {
 	Atom a{1, 1, 2, 3};
+	Atom b{ 2, 87, 4, 2 };
 
-	cout << "Hej, vi har en atom med ID " << a.getID() << ", position (" << a.getPosX() << ", " << a.getPosY() << ", " << a.getPosZ() << ") och hastighet (" << a.getVelocityX() << ", " << a.getVelocityY() << ", " << a.getVelocityZ() << ")!" << endl;
+	cout << "a: ID " << a.getID() << ", position (" << a.getPosX() << ", " << a.getPosY() << ", " << a.getPosZ() << ") och hastighet (" << a.getVelocityX() << ", " << a.getVelocityY() << ", " << a.getVelocityZ() << ")!" << endl;
+	cout << "b: ID " << b.getID() << ", position (" << b.getPosX() << ", " << b.getPosY() << ", " << b.getPosZ() << ") och hastighet (" << b.getVelocityX() << ", " << b.getVelocityY() << ", " << b.getVelocityZ() << ")!" << endl;
 
 
 	a.setPosition(6, 2, 3);
 	a.setVelocity(5, 3, 2);
+	a.addToNeighbourList(b);
+
+	vector<Atom*> anl = a.getNeighbourList();
+	Atom q = *anl[0];
 
 	cout << "Fast nu har vi en atom med ID " << a.getID() << ", position (" << a.getPosX() << ", " << a.getPosY() << ", " << a.getPosZ() << ") och hastighet (" << a.getVelocityX() << ", " << a.getVelocityY() << ", " << a.getVelocityZ() << ")!" << endl;
+	cout << "I dess lista ligger också ett stycke atom med ID " << q.getID() << ", position (" << q.getPosX() << ", " << q.getPosY() << ", " << q.getPosZ() << ") och hastighet (" << q.getVelocityX() << ", " << q.getVelocityY() << ", " << q.getVelocityZ() << ")!" << endl;
 	
 
 	char exit;
