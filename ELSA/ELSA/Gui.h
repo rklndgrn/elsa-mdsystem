98a810d02474f301eb4ea77f9b6ff812cdc69b97
Jonas Ehn <joneh084@student.liu.se>
2018-11-30 14:42:26 +0100

    Cleanup in Gui

--- ELSA/ELSA/Gui.h
+++ ELSA/ELSA/Gui.h
@@ -16,136 +16,150 @@
 #include "imgui-master\imgui_internal.h"
 #include "imgui-master\imgui_impl_glfw.h"
 #include "imgui-master\imgui_impl_opengl3.h"
-//#include "imgui-master\imgui_plot.h"
 
 #pragma once
 
 class Gui
 {
 private:
-	const char* _name;
-	bool _plotVisible;
-	bool _visualVisible;
-	bool _mainVisible;
-	bool _exitPressed;
-	bool _saveResultWindow;
-	bool _loadResultWindow;
-	bool _simulationWindow;
-	float _elementaryCharge = 1.602176620898e-19;
-	
-
-	bool _unableToOpenFile;
-
-	std::string _crystalType;
-	double _temperature;
-	double _timeStep;
-	double _simulationTime;
-	bool _thermostat;
-	bool _useLastSimulationState;
 	bool _2D;
-	double _latticeConstant;
-	double _epsilon;
-	double _sigma;
-	double _cutOffDistance;
-	double _mass;
+	bool _exitPressed;
+	bool _initializing{ false };
+	bool _isVisualisationPause = false;
+	bool _mainVisible;
+	bool _loadResultWindow;
+	bool _plotVisible;
+	bool _saveResultWindow;
+	bool _simulate;
+	bool _simulationWindow;
+	bool _thermostat;
+	bool _unableToOpenFile;
+	bool _useLastSimulationState;
+	bool _visible{ false };
 	bool _visualization;
-	int _numberOfThreads{omp_get_max_threads()};
-	int _numberOfUnitCellsX;
-	int _numberOfUnitCellsY;
-	int _numberOfUnitCellsZ;
-	double _collisionPercentage;
+	bool _visualVisible;
+
+	const char* _name;
 	char* _lastStateFileName;
 
-	int _numberOfTimeStepsPlot;
-
-	bool _simulate;
-	bool _isVisualisationPause = false;
-	int _saveSpeed = 1;
-
-	void printStatistics(double, double, double);
-	void printTimeForVisualisation(int*, int);
-	
-public:
-	Gui();
-	~Gui() = default;
-
-
-	void setupGui(GLFWwindow *window);
-
-	bool _visible{ false };
-	bool _initializing{ false };
-
-	void show();
-	bool visible() const;
-	bool simulate() const;
-	void stopSimulate();
-	void hide();
-
-	static void showHelpMarker(const char* desc);
-
-	std::string getCrystalType();
-	double getTemperature();
-	double getTimeStep();
-	double getSimulationTime();
-	bool isThermostat();
-	bool isUseLastSimulationState();
-	void setMainVisible(bool vis);
-	bool is2D();
-	int getNumberOfUnitCellsX();
-	int getNumberOfUnitCellsY();
-	int getNumberOfUnitCellsZ();
-	int getNumberOfThreads();
-	double getLatticeConstant();
-	double getEpsilon();
-	double getSigma();
-	double getCutOffDistance();
-	double getMass();
-	double getCollisionPercentage();
-	char* getLastStateFileName();
-
-	int _counter;
+	double _collisionPercentage;
+	double _cutOffDistance;
+	double _epsilon;
+	double _latticeConstant;
+	double _mass;
+	double _sigma;
+	double _simulationTime;
+	double _initialTemperature;
+	double _timeStep;
 
 	double* _cohesiveEnergy;
 	double* _debyeTemperature;
 	double* _kineticEnergy;
 	double* _meanSquareDisplacement;
-	double*** _pos;
 	double* _potentialEnergy;
 	double* _pressure;
-	double* _selfDiffusionCoeff;
+	double* _selfDiffusionCoefficient;
 	double* _specificHeat;
-	double* _temp;
+	double* _simulatedTemperature;
 	double* _totalEnergy;
 
+	double*** _positions;
+
+	float _elementaryCharge = 1.602176620898e-19;
+	
+	int _counter;
+	int _numberOfThreads{ omp_get_max_threads() };
+	int _numberOfTimeStepsPlot;
+	int _numberOfUnitCellsX;
+	int _numberOfUnitCellsY;
+	int _numberOfUnitCellsZ;
+	int _saveSpeed = 1;
+
+	std::string _crystalType;
+	
+	void printStatistics(double, double, double);
+	
+public:
+	Gui();
+	~Gui() = default;
+
+	// ------ FUNCTIONS ------
+	
+	bool exitPressed() const;
+	bool isInitializing() const;
+	bool is2D();
+	bool isThermostat();
+	bool isUseLastSimulationState();
 	bool showCrystalSelector(const char* label);
 	bool showMaterialSelector(const char* label);
 	bool showThreadSelector(const char* label);
-
-	void handleFrame();
-
-	void saveResultsWindow();
-	void loadResultsWindow();
-
-	void handleMenu(double, double, int*, int, int*);
-
-	void handleCollapsingHeaders();
-	void handleConfigurationHeader();
-	void handleSettingsHeader();
-
-	void handleProgressBar(double, double);
-
-	void simulateButtonHandler();
-	void stopButtonHandler();
-
-	void Gui::visualWindow(int*, int, int*);
-
-	void simulationWindow(double elapsedTime, double totalTime);
-
+	bool simulate() const;
+	bool visible() const;
 	bool VisualVisible() const;
 
-	bool exitPressed() const;
+	char* getLastStateFileName();
 
+	double getCollisionPercentage();
+	double getCutOffDistance();
+	double getEpsilon();
+	double getLatticeConstant();
+	double getMass();
+	double getSigma();
+	double getSimulationTime();
+	double getTemperature();
+	double getTimeStep();
+
+	double* getCohesiveEnergy() const;
+	double* getDebyeTemperature() const;
+	double* getKineticEnergy() const;
+	double* getMeanSquareDisplacement() const;
+	double* getPotentialEnergy() const;
+	double* getPressure() const;
+	double* getSelfDiffusionCoefficient() const;
+	double* getSpecificHeat() const;
+	double* getSimulatedTemperature() const;
+	double* getTotalEnergy() const;
+
+	double*** getPositions() const;
+
+	int getNumberOfUnitCellsX();
+	int getNumberOfUnitCellsY();
+	int getNumberOfUnitCellsZ();
+	int getNumberOfThreads();
+
+	static void showHelpMarker(const char* desc);
+
+	std::string getCrystalType();
+	
+	void handleCollapsingHeaders();
+	void handleConfigurationHeader();
+	void handleFrame();
+	void handleMenu(double, double, int*, int, int*);
+	void handlePlots();
+	void handleProgressBar(double, double);
+	void handleSettingsHeader();
+	void hide();
+	void loadResultsWindow();
+	void saveResultsWindow();
+	void setCohesiveEnergy(double*);
+	void setDebyeTemperature(double*);
+	void setInitializing(bool);
+	void setKineticEnergy(double*);
+	void setMainVisible(bool vis);
+	void setMeanSquareDisplacement(double*);
 	void setNumberOfTimeStepsPlot(int);
-
-	void handlePlots();// const float* kinEn, const float* totEn, const float* temp);
+	void setPositions(double***);
+	void setPotentialEnergy(double*);
+	void setPressure(double*);
+	void setSelfDiffusionCoefficient(double*);
+	void setSpecificHeat(double*);
+	void setSimulatedTemperature(double*);
+	void setTotalEnergy(double*);
+	void setupGui(GLFWwindow *window);
+	void show();
+	void simulateButtonHandler();
+	void simulationWindow(double elapsedTime, double totalTime);
+	void stopButtonHandler();
+	void stopSimulate();
+	void visualWindow(int*, int, int*);	
 };
